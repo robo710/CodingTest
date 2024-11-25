bf35c79b59735a973769d72de9f3a18227e72445
손찬형 <145670353+robo710@users.noreply.github.com>
2024-11-25 19:56:00 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 61.2 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.kt
+++ 프로그래머스/0/120829. 각도기/각도기.kt
@@ -1,15 +1,9 @@
 class Solution {
-    fun solution(angle: Int): Int {
-        if (angle < 90) {
-            return 1
-        } else if (angle == 90) {
-            return 2
-        } else if (angle < 180) {
-            return 3
-        } else if (angle == 180) {
-            return 4
-        }
-        
-        return 0
+    fun solution(angle: Int) = when{
+        (angle < 90) -> 1
+        (angle == 90)  -> 2
+        (angle < 180) -> 3
+        (angle == 180) -> 4
+        else -> 0
     }
 }
