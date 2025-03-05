278b41e7d38e0497f4a0dc8f04c4e7216e6a3cd8
손찬형 <145670353+robo710@users.noreply.github.com>
2025-03-05 09:48:09 +0900

    [Bronze V] Title: A/B, Time: 116 ms, Memory: 21364 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(){
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+
+    val a = reader.readLine()!!.split(" ").map { it.toDouble() }
+    println(a[0] / a[1])
+}
