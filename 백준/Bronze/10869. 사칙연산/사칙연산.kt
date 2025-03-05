19888e1648ebf91407bedb85427a988e3201b93f
손찬형 <145670353+robo710@users.noreply.github.com>
2025-03-05 16:53:08 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 20596 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,13 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(){
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val a = reader.readLine().split(" ").map { it.toLong() }
+
+    println("${a[0] + a[1]}")
+    println("${a[0] - a[1]}")
+    println("${a[0] * a[1]}")
+    println("${a[0] / a[1]}")
+    println("${a[0] % a[1]}")
+}
