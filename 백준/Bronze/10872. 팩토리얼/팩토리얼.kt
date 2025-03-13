2c98f1775c8b2e8ea84f632a3452ac3cf64beb54
손찬형 <145670353+robo710@users.noreply.github.com>
2025-03-13 19:52:55 +0900

    [Bronze III] Title: 팩토리얼, Time: 76 ms, Memory: 12196 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.kt
@@ -0,0 +1,21 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = reader.readLine().toInt()
+    var answer = 1
+
+    for (i in 1..n) {
+        answer *= i
+    }
+
+    writer.write("$answer")
+
+    writer.flush()
+    writer.close()
+}
