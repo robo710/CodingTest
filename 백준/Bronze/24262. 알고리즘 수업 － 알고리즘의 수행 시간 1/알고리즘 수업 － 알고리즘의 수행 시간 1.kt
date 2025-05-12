ed73dc41cfcb4f42e6e892ca2e683fb5e5c92bb6
손찬형 <145670353+robo710@users.noreply.github.com>
2025-05-12 13:52:43 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 76 ms, Memory: 12216 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val n = reader.readLine().toInt()
+
+    writer.write("1\n")
+    writer.write("0")
+
+    writer.flush()
+    writer.close()
+}
