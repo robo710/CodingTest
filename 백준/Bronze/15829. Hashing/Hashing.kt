30a29d3a442440719e6110303dfffabd2f2f9f65
손찬형 <145670353+robo710@users.noreply.github.com>
2025-05-22 11:18:28 +0900

    [Bronze II] Title: Hashing, Time: 76 ms, Memory: 12256 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -11,14 +11,15 @@ fun main() {
     val a = reader.readLine()
     var answer = 0L
     var pow = 1L
+    val mod = 1234567891
 
-    repeat(n){ i ->
-        answer += (a[i] - 'a' + 1) * pow
-        pow *= 31
+    repeat(n) { i ->
+        val value = a[i] - 'a' + 1
+        answer = (answer + value * pow % mod) % mod
+        pow = (pow * 31) % mod
     }
 
     writer.write("$answer")
-
     writer.flush()
     writer.close()
 }
