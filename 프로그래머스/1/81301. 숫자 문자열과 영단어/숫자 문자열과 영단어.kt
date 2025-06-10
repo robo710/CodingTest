d499d679ad541b939cee45866ee898529e8a7128
손찬형 <145670353+robo710@users.noreply.github.com>
2025-06-10 11:25:38 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 12.14 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -1,55 +1,16 @@
 class Solution {
     fun solution(s: String): Int {
-        var answer: String = ""
-        var i = 0
-        while (i < s.length) {
-            when {
-                s[i] == 'z' -> {
-                    answer += "0"
-                    i += 4
-                }
-                s[i] == 'o' -> {
-                    answer += "1"
-                    i += 3
-                }
-                s[i] == 't' && s[i+1] == 'w' -> {
-                    answer += "2"
-                    i += 3
-                }
-                s[i] == 't' && s[i+1] == 'h' -> {
-                    answer += "3"
-                    i += 5
-                }
-                s[i] == 'f' && s[i+1] == 'o' -> {
-                    answer += "4"
-                    i += 4
-                }
-                s[i] == 'f' && s[i+1] == 'i' -> {
-                    answer += "5"
-                    i += 4
-                }
-                s[i] == 's' && s[i+1] == 'i' -> {
-                    answer += "6"
-                    i += 3
-                }
-                s[i] == 's' && s[i+1] == 'e' -> {
-                    answer += "7"
-                    i += 5
-                }
-                s[i] == 'e' -> {
-                    answer += "8"
-                    i += 5
-                }
-                s[i] == 'n' -> {
-                    answer += "9"
-                    i += 4
-                }
-                else -> {
-                    answer += s[i]
-                    i += 1
-                }
-            }
-        }
-        return answer.toInt()
+        return s.replace("zero", "0")
+            .replace("one", "1")
+            .replace("two", "2")
+            .replace("three", "3")
+            .replace("four", "4")
+            .replace("five", "5")
+            .replace("six", "6")
+            .replace("seven", "7")
+            .replace("eight", "8")
+            .replace("nine", "9")
+            .toInt()
+        
     }
 }
