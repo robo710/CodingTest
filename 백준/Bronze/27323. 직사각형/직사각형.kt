78374f7a9600cde9e11055db1fcd550e16da6ced
손찬형 <145670353+robo710@users.noreply.github.com>
2025-09-15 09:48:54 +0900

    [Bronze V] Title: 직사각형, Time: 72 ms, Memory: 12180 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    val a = reader.readLine().toInt()
+    val b = reader.readLine().toInt()
+
+    writer.write("${a * b}")
+
+    writer.flush()
+    writer.close()
+}
